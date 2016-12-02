bdc21e4dc41620edceb50c6cd6d607c725ce8d5b
Andy Barron <AndrewLBarron@gmail.com>
2016-12-02 00:58:49 -0800

    Switch to Webpack

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
-const STATIC_PATH = path.join(__dirname, 'static');
-const DIST_PATH = path.join(__dirname, 'dist');
-const HTML_PATH = path.join(__dirname, 'index.html');
+const DIST_PATH = path.resolve(__dirname, 'dist');
+const HTML_PATH = path.resolve(__dirname, 'index.html');
 
 const app = express();
-app.use('/static', express.static(STATIC_PATH));
 app.use('/dist', express.static(DIST_PATH));
+app.get('/favicon.ico', (req, res) => res.send(200));
 app.get('*', (req, res) => res.sendFile(HTML_PATH));
 
 app.listen(PORT, () => {
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var path = require('path')
+var webpack = require('webpack')
+
+const INCLUDE_PATHS = [path.resolve(__dirname, 'src')];
+
+module.exports = {
+  entry: ['core-js/shim', './src/node_modules/main.js'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          // vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: INCLUDE_PATHS,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
